4dda18ea33120d7d7e77085ab7a26f910400e034
Evelin Aragon <eve_aragon@hotmail.com>
2020-06-13 17:56:19 -0300

    cambio de Double a String en guest del model

--- src/main/java/com/online/hotel/arlear/model/Guest.java
+++ src/main/java/com/online/hotel/arlear/model/Guest.java
@@ -1,11 +1,10 @@
 package com.online.hotel.arlear.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+
 
 import lombok.Data;
 
@@ -18,8 +17,6 @@ public class Guest {
 	
 	private String name;
 	private String surname;
-	private Double documentNumber;
-	
-	/*@OneToOne(cascade= CascadeType.ALL)
-	private Reservation reservation;*/
+	private String documentNumber;
+
 }
