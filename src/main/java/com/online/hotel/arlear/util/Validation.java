72901e0b74886e77ba221610ab903f5e7506f2c4
umbert oscar <oscar.umbert@gmail.com>
2020-06-15 14:22:12 -0300

    se comenta seña

--- src/main/java/com/online/hotel/arlear/util/Validation.java
+++ src/main/java/com/online/hotel/arlear/util/Validation.java
@@ -635,7 +635,7 @@ public class Validation {
 					errors.add(ErrorMessages.INVALID.getDescription("precio"));
 			}*/
 			
-			if((reservationDto.getSign()==(null))||(reservationDto.getSign()==(""))) {
+			/*if((reservationDto.getSign()==(null))||(reservationDto.getSign()==(""))) {
 				errors.add(ErrorMessages.REQUIRED.getCode());
 				errors.add(ErrorMessages.REQUIRED.getDescription("seña"));
 			}else if(!reservationDto.getSign().toString().matches("\\d+(\\.\\d{1,2})?")){
@@ -644,7 +644,7 @@ public class Validation {
 			}else if((Float.parseFloat(reservationDto.getSign()) < 0)) {
 					errors.add(ErrorMessages.INVALID.getCode());
 					errors.add(ErrorMessages.INVALID.getDescription("seña"));
-			}
+			}*/
 			
 		return errors;
 	}
