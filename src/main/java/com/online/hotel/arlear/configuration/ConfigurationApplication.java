45783d97460970cac8e6cc942f4bf4d5711569d6
nicolencinas <nicolencinas@hotmail.com>
2020-06-15 00:17:24 -0300

    Revierto cambios

--- src/main/java/com/online/hotel/arlear/configuration/ConfigurationApplication.java
+++ src/main/java/com/online/hotel/arlear/configuration/ConfigurationApplication.java
@@ -60,10 +60,7 @@ public class ConfigurationApplication {
 				.allowedOrigins("http://localhost:8090","https://online-hotel-frontend.herokuapp.com")
 				.allowedMethods("GET", "POST", "PUT", "DELETE")
 				.maxAge(3600);
-				registry.addMapping("/survey/**")
-				.allowedOrigins("http://localhost:8090","https://online-hotel-frontend.herokuapp.com")
-				.allowedMethods("GET", "POST", "PUT", "DELETE")
-				.maxAge(3600);
+				
 
 
 
