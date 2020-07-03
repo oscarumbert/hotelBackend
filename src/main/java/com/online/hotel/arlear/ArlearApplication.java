a7ddb3657c25b7b867f4dac2322b5b71e05c619c
umbert oscar <oscar.umbert@gmail.com>
2020-07-03 19:22:15 -0300

    se vuelve atras seguridad

--- src/main/java/com/online/hotel/arlear/ArlearApplication.java
+++ src/main/java/com/online/hotel/arlear/ArlearApplication.java
@@ -70,7 +70,7 @@ public class ArlearApplication {
 		};
 	}
 
-
+/*
 	@EnableWebSecurity
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -141,6 +141,6 @@ public class ArlearApplication {
 				.anyRequest().authenticated();
 		}
 	}
-
+*/
 	
 }
