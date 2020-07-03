268b1309d9736e92643820726ec0dabdd31c7c6f
Gabriel Vasquez Osorio <gabrielito1978@gmail.com>
2020-07-03 17:35:02 -0300

    validacion check-in

--- src/main/java/com/online/hotel/arlear/controller/ReservationController.java
+++ src/main/java/com/online/hotel/arlear/controller/ReservationController.java
@@ -414,36 +414,43 @@ public class ReservationController {
 		if(errors.size()==0) {
 			Reservation reservationInicial=reservationService.findID(id);			
 			if(reservationInicial!=null) {
-				Double sign=reservationInicial.getSign();
-				Double totalPrice=reservationInicial.getPrice();
-				Integer document=reservationInicial.getContact().getDocumentNumber();				
-				if(reservationService.verificateTotalPrice(totalPrice,sign,signRest)) {
-					if(reservationService.verificateCheckIn(id,signRest)) {						
-						Reservation reservationFinal=reservationService.findID(id);
-						if(signRest!=0.0) {
-								TransactiontDTO transaction= new TransactiontDTO();
-								transaction.setDocument(document);
-								transaction.setAmount(signRest);
-								transaction.setElement("Habitacion n°: "+reservationFinal.getRoom().getRoomNumber());
-								transaction.setDescription("Rerserva de Habitación (Pago Total). Id: "+reservationFinal.getId());
-								transaction.setSection(Section.HOTEL);
-								transaction.setTransactionStatus(TransactionStatus.PAGADO.toString());
-								transaction.setNumberSection(reservationFinal.getId());
-								transaction.setDate(java.time.LocalDateTime.now());
-								Transaction transactionModel = objectConverter.converter(transaction);							
-								//Prueba
-								Ticket ticket=ticketService.findByTicketOpen(document);
-								ticket.getTransaction().add(transactionModel);
-								ticketService.update(ticket);
+				//LocalDate hoy=reservationInicial.getBeginDate();
+				LocalDate hoy=LocalDate.now();
+				if ((reservationInicial.getBeginDate().isBefore(hoy)&&reservationInicial.getEndDate().isAfter(hoy))
+				   ||reservationInicial.getBeginDate().equals(hoy)) {
+					Double sign=reservationInicial.getSign();
+					Double totalPrice=reservationInicial.getPrice();
+					Integer document=reservationInicial.getContact().getDocumentNumber();				
+					if(reservationService.verificateTotalPrice(totalPrice,sign,signRest)) {
+						if(reservationService.verificateCheckIn(id,signRest)) {						
+							Reservation reservationFinal=reservationService.findID(id);
+							if(signRest!=0.0) {
+									TransactiontDTO transaction= new TransactiontDTO();
+									transaction.setDocument(document);
+									transaction.setAmount(signRest);
+									transaction.setElement("Habitacion n°: "+reservationFinal.getRoom().getRoomNumber());
+									transaction.setDescription("Rerserva de Habitación (Pago Total). Id: "+reservationFinal.getId());
+									transaction.setSection(Section.HOTEL);
+									transaction.setTransactionStatus(TransactionStatus.PAGADO.toString());
+									transaction.setNumberSection(reservationFinal.getId());
+									transaction.setDate(java.time.LocalDateTime.now());
+									Transaction transactionModel = objectConverter.converter(transaction);							
+									//Prueba
+									Ticket ticket=ticketService.findByTicketOpen(document);
+									ticket.getTransaction().add(transactionModel);
+									ticketService.update(ticket);
+									response=ErrorTools.createOk("el Check-In.");
+							}
+							else{
 								response=ErrorTools.createOk("el Check-In.");
-						}
-						else{
-							response=ErrorTools.createOk("el Check-In.");
+							}
 						}
 					}
-				}
-				else{
-					response= ErrorTools.priceOverange();
+					else{
+						response= ErrorTools.priceOverange();
+					}
+				}else { 
+					response=ErrorTools.createError("el Check-In. Usted reservo de "+reservationInicial.getBeginDate()+" al "+ reservationInicial.getEndDate());
 				}
 			}else{
 				response=ErrorTools.createError("No existe esa Reserva");
--- src/main/java/com/online/hotel/arlear/util/Validation.java
+++ src/main/java/com/online/hotel/arlear/util/Validation.java
@@ -634,7 +634,7 @@ public class Validation {
 			
 		return errors;
 	}
-	public static List<String> applyValidationReservation2(Reservation2DTO reservationDto) {
+	/*public static List<String> applyValidationReservation2(Reservation2DTO reservationDto) {
 		
 		List<String> errors = new ArrayList<String>(); 
 		
@@ -715,7 +715,7 @@ public class Validation {
 			
 			
 		return errors;
-	}
+	}*/
 
 	public static List<String> applyValidationReservationEx2(ReservationEx2DTO reservationDto) {
 		
